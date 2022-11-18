4fc7bda6eed2bbf5fe28ba1330cf1a06f9485c11
Martin Bašniar <m.basniar@gmail.com>
2022-11-18 19:41:14 +0100

    made new structure of States

--- src/components/Meme.jsx
+++ src/components/Meme.jsx
@@ -1,34 +1,50 @@
 import React from "react"
+import {nanoid} from "nanoid"
 
 export default function Meme() {
-
-    const [meme, setMeme] = React.useState({
-        topText: "",
-        topTextPositionX: "0",
-        topTextPositionY: "0",
-        bottomText: "",
-        randomImage: "http://i.imgflip.com/1bij.jpg"
-    })
-
-    const [allMemes, setAllMemes] = React.useState([]);
-
     React.useEffect(() => {
         fetch("https://api.imgflip.com/get_memes")
             .then(res => res.json())
             .then(data => setAllMemes(data.data.memes))
     }, [])
 
+    const [allMemes, setAllMemes] = React.useState([]);
+
+    const [memeImage, setMemeImage] = React.useState({
+        randomImage: "http://i.imgflip.com/1bij.jpg"
+    })
+
     const getMemeImage = () => {
         const randomNumber = Math.floor(Math.random() * allMemes.length);
         const url = allMemes[randomNumber].url;
-        setMeme(prevMeme => ({
+        setMemeImage(prevMeme => ({
             ...prevMeme,
             randomImage: url
         }))
     }
 
+    //Work here
+
+    const [meme, setMeme] = React.useState([{
+        id: nanoid(),
+        text: "",
+        textPositionX: "0",
+        textPositionY: "0",
+    }])
+
+    const addMemeText = () => {
+        const newText = {
+            id: nanoid(),
+            text: "",
+            textPositionX: "0",
+            textPositionY: "0"
+        }
+        setMeme(prevMeme => [newText, ...prevMeme])
+    }
+
+    console.log(meme)
+
     const handleChange = (event) => {
-        console.log(event.target.value)
         const {name, value} = event.target
         setMeme(prevMeme => ({
             ...prevMeme,
@@ -36,46 +52,38 @@ export default function Meme() {
         }))
     }
 
-    //const styles = {
-    //    transform: "translateX(50%)"
-    //}
-
     return (
         <main>
             <p className='getMeme'>Just made screenshot to get Meme!</p>
             <div className="form">
                 <input
                     type="text"
-                    placeholder="Top text"
+                    placeholder="first text"
                     className="form-input"
-                    name="topText"
+                    name="text"
                     onChange={handleChange}
-                    value={meme.topText}
+                    value={meme.text}
                 />
                 <input
                     type="number"
-                    placeholder="Top text position X"
+                    placeholder="first text position X"
                     className="form-input"
-                    name="topTextPositionX"
+                    name="textPositionX"
                     onChange={handleChange}
-                    value={meme.topTextPositionX}
+                    value={meme.textPositionX}
                 />
                 <input
                     type="number"
-                    placeholder="Top text position Y"
+                    placeholder="first text position Y"
                     className="form-input"
-                    name="topTextPositionY"
+                    name="textPositionY"
                     onChange={handleChange}
-                    value={meme.topTextPositionY}
+                    value={meme.textPositionY}
                 />
-                <input
-                    type="text"
-                    placeholder="Bottom text"
-                    className="form-input"
-                    name="bottomText"
-                    onChange={handleChange}
-                    value={meme.bottomText}
-                />
+                <button
+                    onClick={addMemeText}>
+                    plus
+                </button>
                 <button
                     className="form-button"
                     onClick={getMemeImage}
@@ -84,12 +92,11 @@ export default function Meme() {
                 </button>
             </div>
             <div className="meme">
-                <img src={meme.randomImage} className="meme-image" />
+                <img src={memeImage.randomImage} className="meme-image" />
                 <h2 
-                    style={{transform: `translate(${meme.topTextPositionX}px, ${meme.topTextPositionY}px`}}
-                    className="meme-text top">{meme.topText}
+                    style={{transform: `translate(${meme.textPositionX}px, ${meme.textPositionY}px`}}
+                    className="meme-text">{meme.text}
                 </h2>
-                <h2 className="meme-text bottom">{meme.bottomText}</h2>
             </div>
         </main>
     )
