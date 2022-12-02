8e55e58da590b3282100bc593781c83910394b27
Martin Bašniar <m.basniar@gmail.com>
2022-12-02 23:02:41 +0100

    added trash icon for form

--- src/components/Meme.jsx
+++ src/components/Meme.jsx
@@ -40,8 +40,8 @@ export default function Meme() {
 
     const inputElements = memeArray.map(memeObject => {
         return (
-            <div key={memeObject.id} className="row mb-4 mb-md-2 align-items-end">
-                <div className="col-md-6">
+            <div key={memeObject.id} className="row mb-4 mb-md-2 align-items-end justify-content-between">
+                <div className="col-md-7">
                     <label for={memeObject.id} class="form-label">Your text</label>
                     <input
                         type="text"
@@ -54,8 +54,8 @@ export default function Meme() {
                     />
                 </div>
 
-                <div className="mt-1 col-sm-6 col-md-2">
-                    <label for={memeObject.id} class="form-label">Text position X</label>
+                <div className="mt-3 col-6 col-sm-5 col-md-2">
+                    <label for={memeObject.id} class="form-label">Position X</label>
                     <input
                         type="number"
                         className="form-control"
@@ -67,8 +67,8 @@ export default function Meme() {
                     />
                 </div>
 
-                <div className="mt-1 col-sm-6 col-md-2">
-                    <label for={memeObject.id} class="form-label">Text position Y</label>
+                <div className="mt-3 col-6 col-sm-5 col-md-2">
+                    <label for={memeObject.id} class="form-label">Position Y</label>
                     <input
                         type="number"
                         className="form-control"
@@ -80,12 +80,22 @@ export default function Meme() {
                     />
                 </div>
 
-                <div className="mt-3 col-md-2">
+                <div className="mt-3 col-sm-1 col-md-1">
                     <button 
-                        className="btn btn-light"
+                        className="btn btn-secondary"
                         id={memeObject.id}
                         onClick={removeMemeText}>
-                        Delete 
+                        <svg 
+                            xmlns="http://www.w3.org/2000/svg"
+                            width="16"
+                            height="16"
+                            fill="currentColor"
+                            class="bi bi-trash"
+                            viewBox="0 0 16 16"
+                        >
+                            <path d="M5.5 5.5A.5.5 0 0 1 6 6v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5zm2.5 0a.5.5 0 0 1 .5.5v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5zm3 .5a.5.5 0 0 0-1 0v6a.5.5 0 0 0 1 0V6z"/>
+                            <path fill-rule="evenodd" d="M14.5 3a1 1 0 0 1-1 1H13v9a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V4h-.5a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1H6a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1h3.5a1 1 0 0 1 1 1v1zM4.118 4 4 4.059V13a1 1 0 0 0 1 1h6a1 1 0 0 0 1-1V4.059L11.882 4H4.118zM2.5 3V2h11v1h-11z"/>
+                        </svg>
                     </button>
                 </div>
         </div>
@@ -108,10 +118,10 @@ export default function Meme() {
                 {textElements}
             </div>
 
-            <div className="form">
+            <div>
                 {inputElements}
                 <button
-                    className="btn btn-secondary"
+                    className="btn btn-secondary mt-3"
                     onClick={addMemeText}>
                     Add text
                 </button>
